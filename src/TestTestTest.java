6bbfaf67b20544884312c63179f2d5239ed7bd38
mltsvektr <mltsvekaterina@yandex.ru>
2023-07-02 14:12:44 +0300

    last version2

--- src/TestTestTest.java
+++ src/TestTestTest.java
@@ -1,5 +1,9 @@
 public class TestTestTest {
+    public static int lol = 2;
     public static void main(String[] args) {
-        System.out.println("Hello!");
+        for(int i = 0; i < 9; i++){
+            System.out.println(lol);
+            lol <<= 1;
+        }
     }
 }
